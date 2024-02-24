9603aeb66630d633886c950988ab001e7f02d055
mondirhallouli <halloulim@gmail.com>
2024-02-24 15:13:17 +0100

    updated README and added mobile menu

--- README.md
+++ README.md
@@ -1,30 +1,49 @@
-# React + TypeScript + Vite
+# Frontend Mentor - Insure landing page solution
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a solution to the [Insure landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/insure-landing-page-uTU68JV8). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
-Currently, two official plugins are available:
+## Table of contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
 
-## Expanding the ESLint configuration
+## Overview
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+### The challenge
 
-- Configure the top-level `parserOptions` property like this:
+Users should be able to:
 
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+### Screenshot
+
+![](./Screenshot.png)
+
+
+### Links
+
+- Solution URL: [source code](https://github.com/mondirhallouli/insure-lp-fem)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Tailwind CSS](https://www.tailwindcss.com/) - For styles
+
+## Author
+
+- Website - [Mondir Hallouli](https://www.mh-portfolio.pages.dev)
+- Frontend Mentor - [@mondirhallouli](https://www.frontendmentor.io/profile/mondirhallouli)
+- LinkedIn - [@mondirhallouli](https://www.Linkedin.com/in/mondir-hallouli)
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,44 @@
 import logo from '/images/assets/logo.svg';
 import hamburgerIcon from '/images/assets/icon-hamburger.svg';
+import closeIcon from '/images/assets/icon-close.svg';
+import { useState } from 'react';
 
 export default function Header() {
+    const [open, setOpen] = useState(false);
+
     return (
-        <header className='py-6 px-5 z-50 bg-white'>
+        <header className='py-6 px-5 z-[100] bg-white relative'>
+            {/* NAVBAR */}
             <nav className='container mx-auto flex justify-between items-center bg-white z-50'>
                 <a href="/">
                     <img src={logo} alt="insure logo" />
                 </a>
 
-                <section className='hidden md:flex justify-between items-center gap-10'>
-                    <ul className='flex gap-10'>
+                {/* NAVIGATION MENU */}
+                <section className={`pt-8 pb-80 px-5 transition-all w-full absolute left-0 ${!open ? '-top-[700px]' : 'top-20'} bg-veryDarkViolet overflow-hidden md:bg-white md:pt-0 md:pb-0 md:px-0 md:w-auto md:h-auto md:static md:flex md:justify-between md:items-center md:gap-10`}>
+
+                    <ul className='flex flex-col justify-center items-center gap-3 mb-8 md:mb-0 md:gap-10 md:flex-row md:justify-normal'>
                         <li>
-                            <a href="#" className='font-karlaFont font-semibold tracking-widest text-darkGrayishViolet hover:text-veryDarkViolet'>HOW WE WORK</a>
+                            <a href="#" className='inline-block font-karlaFont font-semibold tracking-widest text-veryLightGray text-lg md:text-base md:text-darkGrayishViolet md:hover:text-veryDarkViolet py-2 md:py-0'>HOW WE WORK</a>
                         </li>
                         <li>
-                            <a href="#" className='font-karlaFont font-semibold tracking-widest text-darkGrayishViolet hover:text-veryDarkViolet'>BLOG</a>
+                            <a href="#" className='inline-block font-karlaFont font-semibold tracking-widest text-veryLightGray text-lg md:text-base md:text-darkGrayishViolet md:hover:text-veryDarkViolet py-2 md:py-0'>BLOG</a>
                         </li>
                         <li>
-                            <a href="#" className='font-karlaFont font-semibold tracking-widest text-darkGrayishViolet hover:text-veryDarkViolet'>ACCOUNT</a>
+                            <a href="#" className='inline-block font-karlaFont font-semibold tracking-widest text-veryLightGray text-lg md:text-base md:text-darkGrayishViolet md:hover:text-veryDarkViolet py-2 md:py-0'>ACCOUNT</a>
                         </li>
                     </ul>
 
-                    <button className='py-2 px-6 text-sm tracking-widest border-2 border-black transition-all hover:bg-black hover:text-white'>VIEW PLANS</button>
+                    <button className='inline-block z-50 w-full py-3 px-6 text-veryLightGray font-semibold tracking-widest border-2 border-veryLightGray transition-all hover:bg-veryLightGray hover:text-veryDarkViolet md:text-sm md:text-veryDarkViolet md:font-normal md:border-veryDarkViolet md:hover:bg-veryDarkViolet md:hover:text-white md:py-2 md:w-auto'>VIEW PLANS</button>
+
+                    <div className="z-10 absolute bottom-0 right-0 md:hidden">
+                        <img src="/images/assets/bg-pattern-mobile-nav.svg" className='block w-full' />
+                    </div>
                 </section>
 
-                <button className='md:hidden'>
-                    <img src={hamburgerIcon} alt="hamburger menu button" />
+                {/* MOBILE MENU BUTTON(s) */}
+                <button onClick={() => setOpen(!open)} className='md:hidden'>
+                    <img src={open ? closeIcon : hamburgerIcon} alt="menu button icon" />
                 </button>
             </nav>
         </header>
